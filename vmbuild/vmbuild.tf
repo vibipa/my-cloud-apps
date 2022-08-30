4ba9c7e35f5e87ac285b2c44fc6de2575d14c423
Vibi Peedikayil Abraham <vibipa@gmail.com>
2022-08-30 16:20:17 -0400

    Update vmbuild.tf

--- vmbuild/vmbuild.tf
+++ vmbuild/vmbuild.tf
@@ -1,3 +1,7 @@
+provider "azurerm" {
+    features {}
+}
+
 locals {
   resource_group_name = "rg-visualstudio-lab-june-dev"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDKhIOHFOigOp0Ia9LbbqeuSdcGdqpKat9rFA77MfLjObNlm3erL3v3QY6+YPlQfYy8aTqTKz4GrjRtp84kfTrepLLN+WCSDyaD703F6wAdVYzfKKHx/FaArSHEnJ2ioe8+1BxnSaJziHW7hgEMXNOk7ooIvp3+DaI4K4XcNLrbA3FWkSAgB4xWhXl5LnCOS9iI9jlipBgt3FCQ9WruWl6YomwhtxWTsuog3wt5Jo5sXzF+n63Y/JUN0Ac+N4ya8kpj4yb/rB9w2geBPUMAh1JKIqXK8Z1VgfTEPw311UZmL8UyKhOzU2JspsRMItk6tCMdiTrxllh32Dq2q+T4nzsR vibi@cc-78a5471a-6bc896df5c-4tqzh"
@@ -53,8 +57,8 @@ resource "azurerm_network_interface" "lab_nic" {
     #network_security_group_id = "${azurerm_network_security_group.lab_nsg.id}"
     ip_configuration {
         name = "labNicConfiguration"
-        subnet_id = "${azurerm_virtual_network.lab_vnet.id}"
-        private_ip_address_allocation = "dynamic"
+        subnet_id = "${azurerm_subnet.lab_subnet.id}"
+        private_ip_address_allocation = "Dynamic"
         public_ip_address_id = "${azurerm_public_ip.lab_pip.id}"
     }
 }
